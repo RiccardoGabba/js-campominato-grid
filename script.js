2d8ea88b4fd047d8cedea1a80320d1eb7437e736
filippo <riccardo.gabba02@gmail.com>
2023-10-19 16:06:00 +0200

    visualized the bombs in the index with css background

--- script.js
+++ script.js
@@ -8,21 +8,22 @@ document.addEventListener('DOMContentLoaded', () => {
     //Board creation
     function createBoard() {
 
-        const bombArray = Array(bombAmount).fill('bomb')
+        const bombsArray = Array(bombAmount).fill('bomb')
+
         const emptyArray = Array(width*width - bombAmount).fill('valid')
-        console.log(bombArray)
-        console.log(emptyArray)
 
+        const gameArray = emptyArray.concat(bombsArray)
 
-
+        const shuffledArray = gameArray.sort(() => Math.random () -0.5 )
+       
+ 
         for (let i = 0; i < width*width; i++) {
             const square = document.createElement('div')
             square.setAttribute('id', i)
+            square.classList.add(shuffledArray[i])
             grid.appendChild(square)
             squares.push(square)
         }
-        createBoard()
-
     }
-
+    createBoard()
 }) 
