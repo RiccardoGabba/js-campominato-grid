75ac08e341ed60a45a1b998c90fcc452b9f7bead
filippo <riccardo.gabba02@gmail.com>
2023-10-19 15:52:20 +0200

    help

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const grid = document.querySelector('.grid')
+    let width = 10
+    let bombAmount = 20
+    let squares = []
+
+    //Board creation
+    function createBoard() {
+
+        const bombArray = Array(bombAmount).fill('bomb')
+        const emptyArray = Array(width*width - bombAmount).fill('valid')
+        console.log(bombArray)
+        console.log(emptyArray)
+
+
+
+        for (let i = 0; i < width*width; i++) {
+            const square = document.createElement('div')
+            square.setAttribute('id', i)
+            grid.appendChild(square)
+            squares.push(square)
+        }
+        createBoard()
+
+    }
+
+}) 
